411b4c9db3896291dbdaf594051416bf1a078b38
sarafashka <sarafanova.nv@gmail.com>
2024-07-12 16:15:59 +0300

    fix change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  // base: '/cryptocoin-list',
+  // base: '/',
+  base: '/cryptocoin-list',
   plugins: [react()],
 });
